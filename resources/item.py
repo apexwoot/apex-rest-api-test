4f1d989f4a597e1980d3fa859ad0f0810d4c3255
Vitalii Kohut <vitaliikohut@Vitaliis-MacBook-Pro.local>
2020-07-09 16:11:24 +0200

    Another fix of checking created store

--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,7 @@
 from models.item import ItemModel
 from flask_jwt import jwt_required
 from flask_restful import Resource, reqparse
-from resources.store import StoreList
+from models.store import StoreModel
 
 
 class Items(Resource):
@@ -17,14 +17,13 @@ class Items(Resource):
 
     def post(self, name):
         data = Items.parser.parse_args()
-        if len(StoreList.get()['stores']) < data['store_id']:
-            return {'message': 'check your stores'}
-        else:
+        if StoreModel.query.all().filter_by(id=data['store_id']).first():
             if ItemModel.find_by_name(name):
                 return {'message': 'item "{}" already exists'.format(name)}
             item = ItemModel(name, **data)
             item.save_to_db()
             return item.json()
+        return {'message': 'check your stores'}
     
     def put(self, name):
         data = Items.parser.parse_args()
