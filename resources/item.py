296d646561842b8099ca0a0ab29f5b4a9347a6f3
Vitalii Kohut <vitaliikohut@Vitaliis-MacBook-Pro.local>
2020-07-09 15:25:37 +0200

    Fixed error 500 if create item before store

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 from models.item import ItemModel
 from flask_jwt import jwt_required
 from flask_restful import Resource, reqparse
+from resources.store import StoreList
 
 
 class Items(Resource):
@@ -16,11 +17,14 @@ class Items(Resource):
 
     def post(self, name):
         data = Items.parser.parse_args()
-        if ItemModel.find_by_name(name):
-            return {'message': 'item "{}" already exists'.format(name)}
-        item = ItemModel(name, **data)
-        item.save_to_db()
-        return item.json()
+        if len(StoreList.get()['stores']) < data['store_id']:
+            return {'message': 'check your stores'}
+        else:
+            if ItemModel.find_by_name(name):
+                return {'message': 'item "{}" already exists'.format(name)}
+            item = ItemModel(name, **data)
+            item.save_to_db()
+            return item.json()
     
     def put(self, name):
         data = Items.parser.parse_args()
